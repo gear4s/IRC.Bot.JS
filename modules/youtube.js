082776de7663971c4abc69d58b8902b464dfb354
Aaron Marais <aaro@graphox.us>
2014-11-27 07:30:37 +0200

    added clunky YouTube module time limit

--- modules/youtube.js
+++ modules/youtube.js
@@ -5,6 +5,8 @@ var youtube = function(bot, _, __, modconf) {
     description: "Get youtube video info"
   }
   
+  var us = {};
+  
   modconf = modconf;
 
   function parseVideoURL(url) {
@@ -20,6 +22,33 @@ var youtube = function(bot, _, __, modconf) {
     
     for(var u in t) {
       if(parseVideoURL(t[u])) {
+        if(us[from]) {
+          var d = new Date();
+          if(us[from].amt == 3) {
+            us[from].time.setMinutes(d.getMinutes() + 5);
+            bot.notice(from, "Your permission to use YouTube is now disabled for 5 minutes");
+            
+            us[from].amt = 0;
+            
+            return false;
+          } else {
+            if(us[from].time > d) {
+              var td = us[from].time - d;
+              td -= Math.floor(td / 1000 / 60 / 60) * 1000 * 60 * 60;
+              var mm = Math.floor(td / 1000 / 60);
+              td -= mm * 1000 * 60;
+              var ss = Math.floor(td / 1000);
+              
+              bot.notice(from, "Your permission to use YouTube has been disabled; enabling in " + mm + "min " + ss + "s");
+              return false;
+            } else {
+              us[from].amt += 1;
+            }
+          }
+        } else {
+          us[from] = {amt: 1, time: new Date()};
+        }
+        
         youtube.getById(parseVideoURL(t[u]), function(data) {
           if(data.pageInfo.totalResults == 0) {
             bot.notice(from, "Invalid YouTube ID: " + parseVideoURL(t[u]));
